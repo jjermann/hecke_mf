0488df171311084af6e6f138db67f0396f321ed2
Jonas Jermann <jjermann2@gmail.com>
2014-04-11 15:32:52 +0200

    for importing all relevant classes

---
+++ all.py
@@ -0,0 +1,11 @@
+from hecke_triangle_groups import HeckeTriangleGroup
+
+from graded_ring.py import QMModularFormsRing, QWeakModularFormsRing, QModularFormsRing,\
+                           QCuspFormsRing, MModularFormsRing, WeakModularFormsRing,\
+                           ModularFormsRing, CuspFormsRing
+
+from space          import QMModularForms, QWeakModularForms, QModularForms, QCuspForms,\
+                           MModularForms, WeakModularForms, ModularForms, CuspForms,\
+                           ZeroForm, 
+
+from subspace       import SubSpaceForms
